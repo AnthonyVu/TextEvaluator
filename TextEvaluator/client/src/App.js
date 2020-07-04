c70c49c9d952502b7b30fa473f0082c91abaea3e
Anthony Vu <anthony_vu@live.ca>
2020-07-04 01:01:57 -0700

    redirects to evaluation page after server processes file

--- TextEvaluator/client/src/App.js
+++ TextEvaluator/client/src/App.js
@@ -2,34 +2,68 @@ import React, { useState } from 'react';
 import Dropzone from 'react-dropzone'
 import fileService from './services/fileService'
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch, Route, useHistory
+} from "react-router-dom"
 
 const Header = (props) => (
   <h2 className="header">Text Evaluator</h2>
 )
+const Home = ({ setWords }) => {
+  const history = useHistory()
 
-function App() {
   const handleDrop = acceptedFiles => {
     const formData = new FormData();
     formData.append('file',acceptedFiles[0])
     fileService.uploadFile(formData).then(res => {
-      for (const word in res) {
-        console.log(`${word}: ${res[word]}`);
-      }
+      setWords(res)
+      history.push('/evaluation')
     });
   }
-   
+
+  return (
+    <div>
+      <Dropzone onDrop={handleDrop}>
+      {({ getRootProps, getInputProps }) => (
+        <div {...getRootProps({ className: "dropzone" })}>
+          <input {...getInputProps()} />
+          <p>Drag'n'drop files, or click to select files</p>
+        </div>
+      )}
+    </Dropzone>
+    </div>
+  )
+}
+
+const Evaluation = ({ words }) => {
+  return (
+    <div>
+      {Object.keys(words).map((key, i) => {
+        return <p key={i}>{key}</p>
+      })}
+    </div>
+  )
+
+}
+
+function App() {
+  
+  const [words, setWords] = useState({});
+
   return (
     <div className="App">
       <Header />
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <div {...getRootProps({ className: "dropzone" })}>
-            <input {...getInputProps()} />
-            <p>Drag'n'drop files, or click to select files</p>
-          </div>
-        )}
-      </Dropzone>
+      <Router>
+        <Switch>
+          <Route path="/evaluation">
+            <Evaluation words={words}/>
+          </Route>
+          <Route path="/">
+            <Home setWords={setWords}/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
